8b44f206b3c93faae6bc97c073325e994fee1bd5
Hallvard Lavik <hallvardlavik@gmail.com>
2026-06-03 18:08:18 +0200

    Use fish.

---
+++ fish/config.fish
@@ -0,0 +1,48 @@
+if status --is-login
+    if test -z "$DISPLAY" && test (tty) = "/dev/tty1"
+        exec start-hyprland
+    end
+end
+
+set fish_greeting ""
+function fish_prompt
+    echo (prompt_pwd) '> '
+end
+
+set VISUAL "/usr/bin/vim"
+set EDITOR $VISUAL
+
+if test -f $HOME/.config/fish/secrets.fish
+    source $HOME/.config/fish/secrets.fish
+end
+
+alias ls='ls --color=auto'
+alias grep='grep --color=auto'
+alias screenshot='slurp | grim -g - - | wl-copy'
+alias s='git status'
+alias aa='git add --all'
+alias au='git add --update'
+alias c='git commit -m'
+alias am='git commit --amend --no-edit'
+alias p='git push'
+alias pl='git pull'
+
+function a
+    git add $argv
+end
+
+function b
+    git checkout $argv
+end
+
+alias node='bun'
+
+alias server="ssh -i $LOCAL_SSH"
+alias snuble="ssh -i $REMOTE_SSH"
+
+set -x XDG_CONFIG_HOME "$HOME/.config"
+set -x BUN_INSTALL "$HOME/.bun"
+
+fish_add_path $BUN_INSTALL/bin
+fish_add_path $HOME/.local/bin
+fish_add_path $HOME/code/docker/target/release/
