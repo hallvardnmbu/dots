44262cd62d70cd368d77ba88bfd407cad9389183
Hallvard Lavik <hallvardlavik@gmail.com>
2026-09-05 13:28:56 +0200

    Autostart spotify in second workspace on start.

--- hypr/modules/start.lua
+++ hypr/modules/start.lua
@@ -3,4 +3,7 @@ hl.on("hyprland.start", function()
     hl.exec_cmd("mako")
     hl.exec_cmd("hypridle")
     hl.exec_cmd("$HOME/.config/screen/loop.sh")
+
+    -- open spotify in second workspace
+    hl.exec_cmd("[workspace 2 silent] spotify-launcher")
 end)
